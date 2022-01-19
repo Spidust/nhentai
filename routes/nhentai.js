1e425b00dc288be84b84d48e1b20dc0b87f2ad78
Spidust <93021253+Spidust@users.noreply.github.com>
2022-01-19 11:16:39 +0700

    add search router and complete the get random

--- routes/nhentai.js
+++ routes/nhentai.js
@@ -3,41 +3,69 @@ const nhentai = new API();
 const express = require("express");
 const router = express.Router();
 
+const bookHandler = (book) => {
+  const id = book.id;
+  const title = book.title;
+  let langs = book.languages;
+  langs = langs.map((lang) => lang.name);
+  let tags = book.tags;
+  tags = tags.filter((tag) => tag.type.type != "artist");
+  tags = tags.map((tag) => tag.name);
+  const pages = book.pages.map((page) => nhentai.getImageURL(page));
+  let artist = book.artists;
+  artist = artist.map((tag) => tag.name);
+  const result = {
+    title,
+    langs,
+    tags,
+    pages,
+    artist,
+    id,
+  };
+  return result;
+};
+const searchBookHandler = (book) => {
+    const id = book.id;
+    const title = book.title;
+    let langs = book.languages;
+    langs = langs.map((lang) => lang.name);
+    let tags = book.tags;
+    tags = tags.filter((tag) => tag.type.type != "artist");
+    tags = tags.map((tag) => tag.name);
+    const thumb = nhentai.getImageURL(book.cover)
+    let artist = book.artists;
+    artist = artist.map((tag) => tag.name);
+    const result = {
+      title,
+      langs,
+      tags,
+      thumb,
+      artist,
+      id,
+    };
+    return result;
+  };
 router.get("/random", async (req, res) => {
   nhentai
     .getRandomBook()
     .then((id) => id.id)
     .then((id) =>
       nhentai.getBook(id).then((book) => {
-        const id = book.id
-        const title = book.title;
-        let langs = book.languages;
-        langs = langs.map((lang) => lang.name);
-        let tags = book.tags;
-        tags = tags.filter((tag) => tag.type.type != "artist");
-        tags = tags.map((tag) => tag.name);
-        const pages = [];
-        for(let i in Range(1, book.pages)){
-            pages.push()
-        }
-        let artist = book.artists;
-        artist = artist.map((tag) => tag.name);
-        const result = {
-          title,
-          langs,
-          tags,
-          pages,
-          artist,
-          id,
-        };
+        const result = bookHandler(book);
 
         res.send(result);
       })
     );
 });
-router.get('/search', (req, res) => {
-    nhentai.search(req.query.query, Number(req.query.page)).then(search => {
-        res.send(search)
-    })
-})
+router.get("/search", (req, res) => {
+  nhentai.search(req.query.query, Number(req.query.page)).then((search) => {
+    let books = search.books.map((book) => searchBookHandler(book));
+    let result = {
+        maxPage: search.pages,
+        query: search.query,
+        books
+    }
+    res.send(result);
+  });
+});
 module.exports = router;
